08c01ce0b4ab912ac87af9135bc75bc7f1f951c5
Kyle Chadha <kyle.chadha@gmail.com>
2014-02-05 19:30:19 -0800

    Add CRUD functionality

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,25 +2,50 @@ class OrdersController < ApplicationController
 
   def index
     @orders = Order.all
+
+    # if params[:show] == "completed"
+    #   @orders = Order.where(is_completed: true)
+    # elsif params[:show] == "all"
+    #   @orders = Order.all
+    # else
+    #   @orders = Order.where(is_completed: false)
+    # end
   end
 
   def new
     @order = Order.new
   end
 
+  def edit
+    @order = Order.find(params[:id])
+  end
+
   def create
     @order = Order.new(order_params)
 
     if @order.save
-      flash[:notice] = "Successfully submitted order!"
       redirect_to action: 'index'
+      flash[:notice] = "Successfully submitted order!"
     else
       render action: 'new'
     end
   end
 
-  def edit
+  def update
     @order = Order.find(params[:id])
+
+    if @order.update(order_params)
+      redirect_to action: 'index'
+      flash[:notice] = 'Order was successfully updated!'
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    @order = Order.find(params[:id])
+    @order.destroy
+    redirect_to action: 'index'
   end
 
   private
